534781cffd1a39873fc41c66c2e10b95e29152fd
davidokun <davidokun@gmail.com>
2017-10-27 20:27:36 -0500

    Adding CDI capability to the project

---
+++ src/main/java/com/singletonapps/service/MessageServiceable.java
@@ -0,0 +1,18 @@
+package com.singletonapps.service;
+
+import com.singletonapps.model.Message;
+
+import java.util.List;
+
+public interface MessageServiceable {
+
+    List<com.singletonapps.model.Message> getAllMessages();
+
+    Message getMessage(long messageId);
+
+    Message addMessage(com.singletonapps.model.Message message);
+
+    Message updateMessage(com.singletonapps.model.Message message);
+
+    Message removeMessage(long id);
+}
